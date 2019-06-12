b9eb91d3c10e81276d1d6991397180356d39686f
Josh Connelly <connellyguy@gmail.com>
2019-06-12 04:11:18 +0000

    Continue switch to REST api

--- routes/analytics-dashboard.js
+++ routes/analytics-dashboard.js
@@ -6,43 +6,35 @@ module.exports = {
         let shortavg_query = "SELECT * FROM v_taggregate ORDER BY avg_time_as_it LIMIT 15;";
         let mosttag_query = "SELECT * FROM v_taggregate ORDER BY number_of_its DESC LIMIT 15;";
 
-        // execute query
-        db.query(query, (err, result) => {
+        // execute queries
+        db.query(longtime_query, (err, lt_result) => {
             if (err) {
                 return next(createError(500,err));
             }
-
-            db.query(longtime_query, (err, lt_result) => {
+            db.query(shortavg_query, (err, sa_result) => {
                 if (err) {
                     return next(createError(500,err));
                 }
-                db.query(shortavg_query, (err, sa_result) => {
+                db.query(mosttag_query, (err, mt_result) => {
                     if (err) {
                         return next(createError(500,err));
+                    }   
+                    if(typeof req.userContext != 'undefined') {
+                        res.render('analytics-dashboard.ejs', {
+                            title: 'Welcome to SAS Tag | Charts',
+                            user: req.userContext.userinfo,
+                            longest_time: lt_result,
+                            shortest_avg: sa_result,
+                            most_tags: mt_result,
+                        });
+                    } else {
+                        res.render('analytics-dashboard.ejs', {
+                            title: 'Welcome to SAS Tag | Charts',
+                            longest_time: lt_result,
+                            shortest_avg: sa_result,
+                            most_tags: mt_result,
+                        });
                     }
-                    db.query(mosttag_query, (err, mt_result) => {
-                        if (err) {
-                            return next(createError(500,err));
-                        }   
-                        if(typeof req.userContext != 'undefined') {
-                            res.render('analytics-dashboard.ejs', {
-                                title: 'Welcome to SAS Tag | Charts',
-                                user: req.userContext.userinfo,
-                                tag_history: result,
-                                longest_time: lt_result,
-                                shortest_avg: sa_result,
-                                most_tags: mt_result,
-                            });
-                        } else {
-                            res.render('analytics-dashboard.ejs', {
-                                title: 'Welcome to SAS Tag | Charts',
-                                tag_history: result,
-                                longest_time: lt_result,
-                                shortest_avg: sa_result,
-                                most_tags: mt_result,
-                            });
-                        }
-                    });
                 });
             });
         });
